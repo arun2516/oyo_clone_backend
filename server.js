7522d73be2c523c21ee57a8ea88ab3145e72e67c
arun pandian <toarun25@gmail.com>
2022-03-03 23:38:39 +0530

    add:commit

---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+const cors = require("cors")
+const path = require('path')
+const roomsRoutes = require('./routes/roomsRoute')
+const userRoute = require('./routes/userRoute')
+const bookingsRoute=require('./routes/bookingsRoute')
+const mongoose = require('mongoose')
+
+app.use(cors())
+
+app.use('/api/rooms',roomsRoutes)
+app.use('/api/users' , userRoute)
+app.use('/api/bookings' , bookingsRoute)
+
+if(process.env.NODE_ENV ==='production')
+{
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+        res.sendFile(path.resolve(__dirname  , 'client/build/index.html'))
+
+    })
+}
+
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+  
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, err => {
+  if (err) throw err;
+  console.log('Connected to MongoDB')
+})
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Node JS Server Started`))
